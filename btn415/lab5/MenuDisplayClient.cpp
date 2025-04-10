12960e928d9092b6fae17391a148a27ed9f65355
Leonardo Caceres <leomoca19@gmail.com>
2025-04-10 15:39:01 -0400

    login implemented

--- btn415/lab5/MenuDisplayClient.cpp
+++ btn415/lab5/MenuDisplayClient.cpp
@@ -2,10 +2,17 @@
 #include <iostream>
 #include <winsock2.h>
 
+// #pragma is not supported by my compiler g++
+// I am dissabling the comment if the compiler being used is from Microsoft/Embarcadero 
+// source: https://stackoverflow.com/questions/71831583/ignoring-pragma-comment-ws2-32-lib-wunknown-pragmas
+#if defined(_MSC_VER) || defined(__BORLANDC__)
 #pragma comment(lib, "ws2_32.lib") // Link Winsock library
+#endif
 
 #define PORT 8080
 #define SERVER_ADDRESS "127.0.0.1"
+
+using std::string, std::cin, std::cout, std::cerr, std::endl;
 
 int main() {
     WSADATA wsaData;
@@ -13,7 +20,6 @@ int main() {
 
     SOCKET clientSocket;
     struct sockaddr_in servAddr;
-    char buffer[1024] = { 0 };
 
     // Create socket
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -23,20 +29,32 @@ int main() {
 
     // Connect to server
     if (0 == connect(clientSocket, (struct sockaddr*)&servAddr, sizeof(servAddr))) {
-        // Display menu
-        std::cout << "Choose an option:\n1. Coffee\n2. Tea\nEnter your choice: ";
-        std::string choice;
-        std::cin >> choice;
+        auto receive = [&clientSocket]() -> string{
+            constexpr int sz{1024};
+            char buff[sz]{};
+            int len = recv(clientSocket, buff, sz, 0);
+            return (0 < len ? string(buff, len) : "");
+        };
+        auto send_msg = [&clientSocket](string msg) {
+            return send(clientSocket, msg.c_str(), msg.size(), 0);
+        };
 
-        // Send choice to server
-        send(clientSocket, choice.c_str(), choice.length(), 0);
+        string str;
 
-        // Receive and display response
-        recv(clientSocket, buffer, 1024, 0);
-        std::cout << "Server Response: " << buffer << std::endl;
+        // login
+        while ("Success" != str) {
+            cout << receive(); 
+
+            cin >> str;
+            send_msg(str);
+
+            str = receive();
+            cout << str; 
+        }
+        cout << "Login successfull" << endl;
     }
     else {
-        std::cerr << "connect() failed, errror: " << WSAGetLastError() << std::endl;
+        cerr << "connect() failed, error: " << WSAGetLastError() << endl;
     }
 
     // Close socket
--- btn415/lab5/MenuDisplayServer.cpp
+++ btn415/lab5/MenuDisplayServer.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <map>
 #include "socket.hpp"
-/*#include <winsock2.h>*/
-/**/
-/*#pragma comment(lib, "ws2_32.lib") // Link Winsock library*/
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib") // Link Winsock library
 
 #define PORT 8080
 
-using std::string;
+using std::string, std::cin, std::cout, std::cerr, std::endl;
 
 int main() {
     std::map<string, string> users = {
@@ -16,64 +16,62 @@ int main() {
         { "3", "pswd3" }
     };
 
-    TcpSock server(true), client(false);
-    try {
-        client = server.startServer();
-    }
-    catch (const std::exception& err) {
-        return 1;
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData); // Initialize Winsock
+    
+    SOCKET serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    int addrlen = sizeof(clientAddr);
+    
+    // Create socket
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+    
+    // Bind socket to address
+    bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    
+    // Start listening
+    listen(serverSocket, 3);
+    cout << "Server listening on port " << PORT << "..." << endl;
+    
+    // Accept client connection
+    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrlen);
+    cout << "Client connected" << endl;
+    
+    string str;
+    
+    auto receive = [&clientSocket]() -> string {
+        constexpr int sz{1024};
+        char buff[sz]{};
+        int len = recv(clientSocket, buff, sz, 0);
+        return (0 < len ? string(buff, len) : "");
+    };
+    auto send_msg = [&clientSocket](string msg) {
+        return send(clientSocket, msg.c_str(), msg.size(), 0);
+    };
+
+    // login
+    bool success{};
+    while (!success) {
+        send_msg("Enter id,password: ");
+        str = receive(); //id,password
+
+        const int delim = str.find(',');
+        string id = str.substr(0, delim);
+        string pswd = str.substr(delim + 1, str.size());
+
+        success = users.contains(id) && users[id] == pswd;
+        send_msg(!success ? "Fail\n" : "Success");
     }
 
-    client.send("Hello from server\n");
-    std::cout << client.recv();
+    cout << "Closing server" << endl;
 
-    /*WSADATA wsaData;*/
-    /*WSAStartup(MAKEWORD(2, 2), &wsaData); // Initialize Winsock*/
-    /**/
-    /*SOCKET serverSocket, clientSocket;*/
-    /*struct sockaddr_in serverAddr, clientAddr;*/
-    /*int addrlen = sizeof(clientAddr);*/
-    /*char buffer[1024] = { 0 };*/
-    /**/
-    /*// Create socket*/
-    /*serverSocket = socket(AF_INET, SOCK_STREAM, 0);*/
-    /*serverAddr.sin_family = AF_INET;*/
-    /*serverAddr.sin_addr.s_addr = INADDR_ANY;*/
-    /*serverAddr.sin_port = htons(PORT);*/
-    /**/
-    /*// Bind socket to address*/
-    /*bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));*/
-    /**/
-    /*// Start listening*/
-    /*listen(serverSocket, 3);*/
-    /*std::cout << "Server listening on port " << PORT << "..." << std::endl;*/
-    /**/
-    /*// Accept client connection*/
-    /*clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrlen);*/
-    /**/
-    /*// Receive data from client*/
-    /*recv(clientSocket, buffer, 1024, 0);*/
-    /*string clientChoice(buffer);*/
-    /*string response;*/
-    /**/
-    /*// Process client choice*/
-    /*if (clientChoice == "Coffee") {*/
-    /*    response = "Your coffee is being prepared!";*/
-    /*}*/
-    /*else if (clientChoice == "Tea") {*/
-    /*    response = "Your tea is being prepared!";*/
-    /*}*/
-    /*else {*/
-    /*    response = "Invalid choice";*/
-    /*}*/
-    /**/
-    /*// Send response to client*/
-    /*send(clientSocket, response.c_str(), response.size(), 0);*/
-    /**/
-    /*// Close sockets*/
-    /*closesocket(clientSocket);*/
-    /*closesocket(serverSocket);*/
-    /*WSACleanup();*/
+    // Close sockets
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
 
     return 0;
 }
